b694e4b8aa6266f5e43fb6309b1e31bd79f63552
Alessandro Cavaleri <caval1289@gmail.com>
2019-09-05 17:30:46 +0200

    tableau

--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import SearchBar from '../containers/search-bar'
+import RateExchangeList from '../containers/rate_exchange_list';
 
 export default class App extends Component {
   render() {
     return (
       <div>
-        <SearchBar/>
+        <SearchBar />
+        <RateExchangeList />
       </div>
     );
   }
