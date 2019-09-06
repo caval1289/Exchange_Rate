e703ca83a5c1c35e2a272a7068ddc0e368900660
Alessandro Cavaleri <caval1289@gmail.com>
2019-09-06 19:42:53 +0200

    fin du projet

--- src/components/rate_exchange_list_item.js
+++ src/components/rate_exchange_list_item.js
@@ -1,14 +1,34 @@
 import React, { Component } from 'react';
+import ReactChartKick, { AreaChart } from 'react-chartkick';
+import Chart from 'chart.js';
 
+ReactChartKick.addAdapter(Chart);
 class RateExchangeListItem extends Component {
     render() {
+        console.log(this.props.rateExchange);
+        const { flag, name, rates, currencyCode } = this.props.rateExchange;
         return (
             <tr>
-                <td> Pays </td>
-                <td> Graphique </td>
+                <td> {name}
+                    <br />
+                    <img src={flag} height="60" width="100" />
+                </td>
+                <td className="col-md-12">
+                    <AreaChart
+                        data={formatData(rates, currencyCode)}
+                        xtitle="Date"
+                        ytitle={currencyCode}
+                    />
+                </td>
             </tr>
         )
     }
+}
+
+function formatData(rates, currencyCode) {
+    return Object.keys(rates).map(date => {
+        return [date, rates[date][currencyCode]];
+    });
 }
 
 export default RateExchangeListItem;
